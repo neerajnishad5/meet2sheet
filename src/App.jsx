5168ec1a4ed8455201158c072044d0edc56f33d1
Neeraj Nishad <neerajnishad5@gmail.com>
2025-08-25 11:26:01 +0530

    feat: added analytics for website on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import routes from "./routes";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <Router>
       <Header />
       <div className="bg-[#fff5f3] flex flex-col items-center justify-center p-6">
+        <Analytics />
         <Routes>
           {routes.map(({ path, element }, index) => (
             <Route key={index} path={path} element={element} />
